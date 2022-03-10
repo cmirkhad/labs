6954c4394b46d0d0724b084713c2cc8a11bde61e
Mirkhad <cmirkhad@mail.ru>
2022-03-10 10:55:33 +0600

    lab 2 done

--- Lab2/Lab2/Form1.Designer.cs
+++ Lab2/Lab2/Form1.Designer.cs
@@ -30,35 +30,62 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(369, 123);
+            this.button1.Location = new System.Drawing.Point(127, 80);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(503, 56);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "MessageBox.Show(string)";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(369, 201);
+            this.button2.Location = new System.Drawing.Point(127, 158);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.Size = new System.Drawing.Size(503, 56);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "MessageBox.Show(string, string)";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(127, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(503, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "MessageBox.Show(string, string, MessageBoxButtons)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(127, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(503, 56);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "MessageBox.Show(string, string, MessageBoxButtons, MessageBoxIcon)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1061, 505);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -67,5 +94,7 @@
 
         private Button button1;
         private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- Lab2/Lab2/Form1.cs
+++ Lab2/Lab2/Form1.cs
@@ -6,5 +6,58 @@ namespace Lab2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a line");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with line", "Line Title");
+            // the first parameter specifies the dialog box text, the second parameter specifies the title text
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a string and different buttons", "Title string", MessageBoxButtons.OKCancel);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.AbortRetryIgnore);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.YesNo);
+            // MessageBoxButtons determines which buttons will be located on the dialog box
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a line and different buttons", "Title bar", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.OKCancel, MessageBoxIcon.Stop);
+            // MessageBoxIcon defines the dialog box icon, the data is in the Enum enumeration type
+        }
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Save data", "Attention!", MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.OK)
+                MessageBox.Show("Data saved");
+            if (result == DialogResult.No)
+                MessageBox.Show("Data not saved");
+            if (result == DialogResult.Cancel)
+                MessageBox.Show("Saving data canceled by the user");
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.Controls.Count; i++)
+            {
+                // Looking for Button controls 
+                if (Convert.ToString(this.Controls[i].GetType().Name) == "Button")
+                {
+                    MessageBox.Show(this,
+                      Convert.ToString(this.Controls[i].Name),
+                      "What buttons are there?");
+                }
+            }
+        }
+
     }
 }
